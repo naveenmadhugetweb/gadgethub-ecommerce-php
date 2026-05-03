f63a181dde9b1232051acdee13e4a4977bb1cf5e
Naveen Vision <naveenvisionmpvkn@gmail.com>
2026-05-02 21:57:15 -0700

    folder structure and some view files created

---
+++ includes/scripts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- <title>Document</title> -->
+
+    <!-- Bootstrap Js CDN -->
+    <script src="/assets/vendors/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendors/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendors/js/popper.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="/assets/js/app.js"></script>
+    <!-- Validation JS -->
+    <script src="../assets/js/validation.js"></script>
+
+
+</head>
+<body>
+    
+</body>
+</html>
