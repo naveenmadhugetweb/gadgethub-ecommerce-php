8db9a6452124f7b58d3b2e13eff3e5a643101e2e
Naveen Vision <naveenvisionmpvkn@gmail.com>
2026-05-05 06:59:00 -0700

    forgot feature added without code verification

--- auth/forgot-password.php
+++ auth/forgot-password.php
@@ -1,3 +1,18 @@
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/auth_functions.php'; ?>
+<?php
+        session_start();
+ ?>
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST['email'];
+        forgotPassword($email);
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +29,17 @@
 
     <h2 class="text text-primary text-center">Forgot Password</h2>
     <p class="text-center">Enter your email address</p>
-    <form method="post" action="reset-password.php" id="loginForm">
+
+    <?php if (isset($_SESSION['error'])): ?>
+        <div class="alert alert-danger">
+            <?= $_SESSION['error']; ?>
+        </div>
+    <?php unset($_SESSION['error']); endif; ?>
+
+    <form method="post" action="forgot-password.php" id="loginForm">
         <div class="form-group">
             <label class="mt-2">Email address</label>
-            <input type="email" class="form-control mt-2" require>
+            <input type="email" name="email" class="form-control mt-2" require>
         </div>
 
         <button class="btn btn-primary d-block w-100 mt-2 fw-bold"> Continue </button>
